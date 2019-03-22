4cf93fd25f19b7cb844db9c86f68a54d8b3604d8
DianaLupasco <47533333+DianaLupasco@users.noreply.github.com>
2019-03-22 13:38:54 +0300

    Add files via upload

---
+++ lucrare 2 web_Lupasco/js/main.js
@@ -0,0 +1,41 @@
+
+
+function sum() {
+    let a1 = document.getElementById('x').value;
+    let a2 = document.getElementById('y').value;
+    a1 = Number.parseInt(a1);
+    a2 = Number.parseInt(a2);
+    a2 += a1;
+    let rez = document.getElementById("rez").innerText = a2;
+  }   
+  
+  document.getElementById("sum").onclick = function() {
+    sum();
+
+     //PUNCTU 3
+
+    let block = documen.querySelector('#container');
+    block = addEventListener('mousemove', function(){
+    block = classList.add('red');
+})
+
+    block = addEventListener('mouselive', function(){
+    block = classList.remove('red');
+  })
+  //PUNCTU 4
+      let paragraph = document.getElementById("paragraf").children;
+      let i;
+      for (i = 0; i < paragraph.length; i++) {
+        if (i % 2 == 0) {
+          paragraph[i].style.backgroundColor = "yellow";
+        } else {
+          paragraph[i].style.backgroundColor = "blue";
+        }
+        
+        };
+
+        
+    }
+ console.log('dfd')    
+    
+   
